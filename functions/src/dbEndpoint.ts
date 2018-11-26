e63ca4b5f1d7afa6e96aeef180d4df893de1f0d9
gewenyu99 <wenyu1999@hotmail.com>
2018-11-25 21:23:00 -0500

    Added endpoints to handle querying of single white and black cards

--- functions/src/dbEndpoint.ts
+++ functions/src/dbEndpoint.ts
@@ -17,14 +17,11 @@ export const getPack = functions.https.onRequest((request, response) => {
     console.log(requestedCardPack);
     if (requestedCardPack === null || requestedCardPack === undefined) {
         response.status(400).send('card-pack-name not found in request headers');
-        console.log(requestedCardPack);
-        console.log('123');
         return
     }
 
     marmosadCards.doc('valid-card-packs').get().then((doc) => {
         if (doc.exists) {
-            console.log(doc.data());
             if (doc.data()[requestedCardPack]) {
                 return true
             }
@@ -46,12 +43,87 @@ export const getPack = functions.https.onRequest((request, response) => {
                     whiteCardCount = collection.size;
                     return
                 }).then(() => {
-                    response.send({
+                    response.status(200).send({
                         message: 'Successfully returned ' + requestedCardPack,
                         responseObj: {whiteCardCount: whiteCardCount, blackCardCount: blackCardCount}
-                    })
+                    });
+                    return
                 })
             });
+        }
+        return
+    });
+});
+
+export const getWhiteCard = functions.https.onRequest((request, response) => {
+    let requestedCardPack = request.get('card-pack-name');
+    let requestedCardId = request.get('card-id');
+    if (requestedCardPack === null || requestedCardPack === undefined) {
+        response.status(400).send('card-pack-name not found in request headers');
+        return
+    }
+    if (requestedCardId === null || requestedCardId === undefined) {
+        response.status(400).send('card-id not found in request headers');
+        return
+    }
+    marmosadCards.doc('valid-card-packs').get().then((doc) => {
+        if (doc.exists) {
+            if (doc.data()[requestedCardPack]) {
+                return true
+            }
+            else {
+                response.status(400).send('requested card pack: ' + requestedCardPack + ' was not found');
+                return false
+            }
+        } else {
+            response.status(500).send('card pack validation failed, no card pack metadata found');
+            return false
+        }
+    }).then((success) => {
+        if (success) {
+            marmosadCards.doc(requestedCardPack).collection('white-cards').doc(requestedCardId).get().then((doc => {
+                response.status(200).send({message: 'Successfully returned white card from ' + requestedCardPack,
+                    responseObj: {cardId: doc.data().cardId, body: doc.data().body}});
+            })).catch(reason => {
+                response.status(400).send('requested card does not exist');
+            })
+        }
+        return
+    });
+});
+
+export const getBlackCard = functions.https.onRequest((request, response) => {
+    let requestedCardPack = request.get('card-pack-name');
+    let requestedCardId = request.get('card-id');
+    if (requestedCardPack === null || requestedCardPack === undefined) {
+        response.status(400).send('card-pack-name not found in request headers');
+        return
+    }
+    if (requestedCardId === null || requestedCardId === undefined) {
+        response.status(400).send('card-id not found in request headers');
+        return
+    }
+    marmosadCards.doc('valid-card-packs').get().then((doc) => {
+        if (doc.exists) {
+            if (doc.data()[requestedCardPack]) {
+                return true
+            }
+            else {
+                response.status(400).send('requested card pack: ' + requestedCardPack + ' was not found');
+                return false
+            }
+        } else {
+            response.status(500).send('card pack validation failed, no card pack metadata found');
+            return false
+        }
+    }).then((success) => {
+        if (success) {
+            marmosadCards.doc(requestedCardPack).collection('black-cards').doc(requestedCardId).get().then((doc => {
+                response.status(200).send({message: 'Successfully returned black card from' + requestedCardPack,
+                    responseObj: {cardId: doc.data().cardId, body: doc.data().body}});
+            })).catch(reason => {
+                response.status(400).send('requested card does not exist');
+            })
         }
         return
     });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,2 +1,4 @@
 import * as dbEndpoints from './dbEndpoint';
 export const getPack = dbEndpoints.getPack;
+export const getWhiteCard = dbEndpoints.getWhiteCard;
+export const getBlackCArd = dbEndpoints.getBlackCard;
